d16386a964860933467e56df1ad082e0530f6f3a
Cindoushka <cindie-baj@live.fr>
2023-07-15 21:52:41 +0200

    new modif, hover and co

--- navigation.js
+++ navigation.js
@@ -13,29 +13,29 @@ window.addEventListener("scroll", function() {
     });
   });
 
-  window.addEventListener("wheel", function(event) {
-    event.preventDefault(); // Empêche le défilement automatique
+  // window.addEventListener("wheel", function(event) {
+  //   event.preventDefault(); // Empêche le défilement automatique
   
-    var sections = document.querySelectorAll(".section");
-    var windowHeight = window.innerHeight;
-    var currentSectionIndex = -1;
+  //   var sections = document.querySelectorAll(".section");
+  //   var windowHeight = window.innerHeight;
+  //   var currentSectionIndex = -1;
   
-    // Trouver l'index de la section actuellement visible
-    sections.forEach(function(section, index) {
-      var rect = section.getBoundingClientRect();
-      if (rect.top >= 0 && rect.top <= windowHeight) {
-        currentSectionIndex = index;
-      }
-    });
+  //   // Trouver l'index de la section actuellement visible
+  //   sections.forEach(function(section, index) {
+  //     var rect = section.getBoundingClientRect();
+  //     if (rect.top >= 0 && rect.top <= windowHeight) {
+  //       currentSectionIndex = index;
+  //     }
+  //   });
   
-    // Vérifier la direction du défilement (vers le haut ou vers le bas)
-    if (event.deltaY < 0 && currentSectionIndex > 0) {
-      // Défilement vers la section précédente
-      var prevSectionOffset = sections[currentSectionIndex - 1].offsetTop;
-      window.scrollTo({ top: prevSectionOffset, behavior: "smooth" });
-    } else if (event.deltaY > 0 && currentSectionIndex < sections.length - 1) {
-      // Défilement vers la section suivante
-      var nextSectionOffset = sections[currentSectionIndex + 1].offsetTop;
-      window.scrollTo({ top: nextSectionOffset, behavior: "smooth" });
-    }
-  });
+  //   // Vérifier la direction du défilement (vers le haut ou vers le bas)
+  //   if (event.deltaY < 0 && currentSectionIndex > 0) {
+  //     // Défilement vers la section précédente
+  //     var prevSectionOffset = sections[currentSectionIndex - 1].offsetTop;
+  //     window.scrollTo({ top: prevSectionOffset, behavior: "smooth" });
+  //   } else if (event.deltaY > 0 && currentSectionIndex < sections.length - 1) {
+  //     // Défilement vers la section suivante
+  //     var nextSectionOffset = sections[currentSectionIndex + 1].offsetTop;
+  //     window.scrollTo({ top: nextSectionOffset, behavior: "smooth" });
+  //   }
+  // });
